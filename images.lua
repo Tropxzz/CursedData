ce321517585efc1d5d2ca703f85ea093ccc7fc9a
Tropxzz <130433432+Tropxzz@users.noreply.github.com>
2023-06-29 19:36:08 +1000

    Update images.lua

--- images.lua
+++ images.lua
@@ -1,4 +1,5 @@
-    home = 7733960981;
+-- This isnt used in the loadstring (I tried and failed) So take these (Btw I used in the script)
+home = 7733960981;
     settings = 7734053495;
     misc = 7733701545;
     general = 7733749837;
